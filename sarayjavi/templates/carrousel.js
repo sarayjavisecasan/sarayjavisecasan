7f5f19aaba5dedb93567b583d99bd8e5d91528cb
sarayjavisecasan <bentitrenta@gmail.com>
2025-05-10 06:27:58 +0200

    Delete sarayjavi directory

--- sarayjavi/templates/carrousel.js
+++
@@ -1,6 +0,0 @@
-const velocidad = 100; // menor = más rápido (en segundos)
-
-const carrusel = document.getElementById('carrusel');
-const totalWidth = carrusel.scrollWidth / 2;
-
-carrusel.style.animationDuration = `${velocidad}s`;
